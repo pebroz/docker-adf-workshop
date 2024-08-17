af8ff2ce99feb56788afd224dee804c4538f2d1b
Per-Erik Broz <per-erik.broz@idrettsforbundet.no>
2024-08-17 22:16:34 +0200

    feat: Text display and mountable folders

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -2,10 +2,11 @@
 #shellcheck shell=sh
 
 set -xe
-rm -Rf ~/.wine
+export WINEPREFIX="/config/.wine"
 
-mkdir -p ~/.wine
-chown -R $USER: ~/.wine
+echo "Installing MS RichEdit Control 2.0 and Courier Fonts with Winetricks for proper ANSII text display, this may take a while..."
+winetricks riched20 courier
+
 cd /opt/ADF-Workshop
 
 # exec xterm
